c4de144e6b1fcd2f02142d1f2e85f9f399e51b63
KaiHicks <hicksk5@wwu.edu>
2019-04-11 12:37:13 -0700

    Added arguments to the hello world to change who you are greeting

--- src/main/java/lab1/Hello.java
+++ src/main/java/lab1/Hello.java
@@ -2,6 +2,15 @@ package lab1;
 
 public class Hello{
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        String s = "World";
+
+        if(args.length >= 1) {
+            s = args[0];
+            for(int i = 1; i < args.length; i++) {
+                s += " " + args[i];
+            }
+        }
+
+        System.out.println("Hello " + s + "!");
     }
 }
